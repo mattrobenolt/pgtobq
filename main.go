77732682d815ef31bdd7761562631af3396414fd
Damon <damonpetta@users.noreply.github.com>
2017-11-25 18:01:24 +0000

    add support for smallint and double precision data types (#1)
    * add support for smallint and double precision data types
    
    * add "time without time zone" to time swtich statement
    
    * fix indentation

--- main.go
+++ main.go
@@ -40,17 +40,17 @@ func (c *Column) ToFieldSchema() *bigquery.FieldSchema {
 	switch c.Type {
 	case "character varying", "text":
 		f.Type = bigquery.StringFieldType
-	case "integer", "bigint":
+	case "integer", "bigint", "smallint":
 		f.Type = bigquery.IntegerFieldType
-	//case "":
-	//	f.Type = bigquery.FloatFieldType
+	case "double precision":
+		f.Type = bigquery.FloatFieldType
 	case "boolean":
 		f.Type = bigquery.BooleanFieldType
 	case "timestamp with time zone":
 		f.Type = bigquery.TimestampFieldType
 	case "date":
 		f.Type = bigquery.DateFieldType
-	case "timestamp", "timestamp without time zone":
+	case "timestamp", "timestamp without time zone", "time without time zone":
 		f.Type = bigquery.TimeFieldType
 	default:
 		// TODO: return as error
